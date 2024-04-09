548ceab0caedbbd912d4b5efe4ec6201b637d32d
asillers <asillers@ucdavis.edu>
2024-04-09 14:50:30 -0700

    added and updated scripts

--- intersect.sh
+++ intersect.sh
@@ -13,10 +13,10 @@
 
 module load bedtools2
 
-bedtools intersect -wa -a farr1.genes.bed -b nanl.bed > nanl_genes.bed
+bedtools intersect -wa -a farr1.gene_models_updated.gff -b nanl.bed | grep 'gene' | awk -F'\t|=|;' '{print$10}' > nanl_genes.txt
 
-bedtools intersect -wa -a farr1.genes.bed -b cnl.bed > cnl_genes.bed
+bedtools intersect -wa -a farr1.gene_models_updated.gff -b cnl.bed | grep 'gene'  | awk -F'\t|=|;' '{print$10}' > cnl_genes.txt
 
-bedtools intersect -wa -a farr1.genes.bed -b tnl.bed > tnl_genes.bed
+bedtools intersect -wa -a farr1.gene_models_updated.gff -b tnl.bed | grep 'gene' | awk -F'\t|=|;' '{print$10}' > tnl_genes.txt
 
-bedtools intersect -wa -a farr1.genes.bed -b xnl.bed > xnl_genes.bed
+bedtools intersect -wa -a farr1.gene_models_updated.gff -b xnl.bed | grep 'gene' | awk -F'\t|=|;' '{print$10}' > xnl_genes.txt
