ac4939591cc6e4ff268468e39e89fffac349d0f8
asillers <asillers@ucdavis.edu>
2025-01-16 16:28:19 -0800

    removed haplotype-phased analyses

--- nlr.sh
+++ nlr.sh
@@ -13,8 +13,4 @@
 
 module load jdk
 
-#java -jar NLR-Annotator/NLR-Annotator-v2.1b.jar -i ../Genome/Nomenclature_Update/farr1_phase1_updated.fa -x NLR-Annotator/src/mot.txt -y NLR-Annotator/src/store.txt -o nlr.hapa.out.txt -g nlr.hapa.out.gff -a hapa.motif.msa.fa
-
-#java -jar NLR-Annotator/NLR-Annotator-v2.1b.jar -i ../Genome/Nomenclature_Update/farr1_phase2_updated.fa -x NLR-Annotator/src/mot.txt -y NLR-Annotator/src/store.txt -o nlr.hapb.out.txt -g nlr.hapb.out.gff -a hapb.motif.msa.fa
-
 java -jar NLR-Annotator/NLR-Annotator-v2.1b.jar -i ../Genome/Nomenclature_Update/farr1_updated.fa -x NLR-Annotator/src/mot.txt -y NLR-Annotator/src/store.txt -o nlr.out.txt -g nlr.out.gff -a nlr.motif.msa.fa -f ../Genome/Nomenclature_Update/farr1_updated.fa nlrs.fa 0 -t 4
