6f314971c92a05f5dfee2a6938cb0fd6da8a6cf4
asillers <asillers@ucdavis.edu>
2025-02-11 15:55:37 -0800

    different settings to get more info

--- intersect.sh
+++ intersect.sh
@@ -13,8 +13,12 @@
 
 module load bedtools2
 
-bedtools intersect -wa -a ../Genome/farr1.gene_models.gff -b nanl.bed | grep 'gene' | awk -F'\t|=|;' '{OFS="\t"} {print $1, $4, $5, $10}' > nanlgenes.bed
-bedtools intersect -wa -a ../Genome/farr1.gene_models.gff -b cnl.bed | grep 'gene' | awk -F'\t|=|;' '{OFS="\t"} {print $1, $4, $5, $10}' > cnlgenes.bed
-bedtools intersect -wa -a ../Genome/farr1.gene_models.gff -b tnl.bed | grep 'gene' | awk -F'\t|=|;' '{OFS="\t"} {print $1, $4, $5, $10}' > tnlgenes.bed
-bedtools intersect -wa -a ../Genome/farr1.gene_models.gff -b xnl.bed | grep 'gene' | awk -F'\t|=|;' '{OFS="\t"} {print $1, $4, $5, $10}' > xnlgenes.bed
+#bedtools intersect -wa -a ../Genome/farr1.gene_models.gff -b nanl.bed | grep 'gene' | awk -F'\t|=|;' '{OFS="\t"} {print $1, $4, $5, $10}' > nanlgenes.bed
+#bedtools intersect -wa -a ../Genome/farr1.gene_models.gff -b cnl.bed | grep 'gene' | awk -F'\t|=|;' '{OFS="\t"} {print $1, $4, $5, $10}' > cnlgenes.bed
+#bedtools intersect -wa -a ../Genome/farr1.gene_models.gff -b tnl.bed | grep 'gene' | awk -F'\t|=|;' '{OFS="\t"} {print $1, $4, $5, $10}' > tnlgenes.bed
+#bedtools intersect -wa -a ../Genome/farr1.gene_models.gff -b xnl.bed | grep 'gene' | awk -F'\t|=|;' '{OFS="\t"} {print $1, $4, $5, $10}' > xnlgenes.bed
 
+bedtools intersect -wao -a nanl.bed -b ../Genome/farr1.gene_models.gff | grep 'gene' > nanlgeneinfo.bed
+bedtools intersect -wao -a cnl.bed -b ../Genome/farr1.gene_models.gff | grep 'gene' > cnlgeneinfo.bed
+bedtools intersect -wao -a tnl.bed -b ../Genome/farr1.gene_models.gff | grep 'gene' > tnlgeneinfo.bed
+bedtools intersect -wao -a xnl.bed -b ../Genome/farr1.gene_models.gff | grep 'gene' > xnlgeneinfo.bed
