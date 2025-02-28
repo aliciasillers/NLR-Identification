cb9cb00f5461c908e65515992350281c44b3332d
asillers <asillers@ucdavis.edu>
2025-02-27 16:56:49 -0800

    added RNLs

--- nlrclass.R
+++ nlrclass.R
@@ -31,9 +31,9 @@ write.table(cnlbed, "cnl.bed", sep = '\t', row.names = FALSE, col.names = FALSE,
 write.table(tnlbed, "tnl.bed", sep = '\t', row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 #bar graph for gene types
-nlrloci <- data.frame(Category = as.factor(c("CNL", "TNL", "NNL", "Partial")), Number = as.numeric(c(length(cnl$V1), length(tnl$V1), length(nnl$V1), length(partial$V1))))
+nlrloci <- data.frame(Category = as.factor(c("CNL", "TNL", "RNL", "NNL", "Partial")), Number = as.numeric(c(length(cnl$V1), length(tnl$V1), 99, length(nnl$V1), length(partial$V1))))
 
 png("nlrlociclass.png", width = 20, height = 20, units = "cm", res = 900)
-plot1 <- ggplot(data = nlrloci, aes(x = factor(Category, level=c('CNL', 'TNL', 'NNL', 'Partial')), y = Number, fill = Category)) + geom_col() + scale_fill_manual(values = c(CNL = "thistle", TNL = "tomato", NNL = "lightgoldenrod1", Partial = "lightblue")) + theme_classic() + theme(axis.title.x=element_blank()) + theme(legend.position="none")
+plot1 <- ggplot(data = nlrloci, aes(x = factor(Category, level=c('CNL', 'TNL', 'RNL', 'NNL', 'Partial')), y = Number, fill = Category)) + geom_col() + scale_fill_manual(values = c(CNL = "thistle", TNL = "tomato", RNL = "lavenderblush", NNL = "lightgoldenrod1", Partial = "lightblue")) + theme_classic() + theme(axis.title.x=element_blank()) + theme(legend.position="none")
 print(plot1)
 dev.off()
