8743d7041bdc931aad526e5a53a63f8643ff9ff4
asillers <asillers@ucdavis.edu>
2025-06-25 13:57:06 -0700

    minor changes to header

--- align.sh
+++ align.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1 # Ensure that all cores are on one machine
 #SBATCH --mem=30G # Memory pool for all cores in MB (see also --mem-per-cpu)
 #SBATCH --partition=bmh # Partition to submit to
-#SBATCH --output=MSA.out # File to which STDOUT will be written
-#SBATCH --error=MSA.err # File to which STDERR will be written
+#SBATCH --output=align.out # File to which STDOUT will be written
+#SBATCH --error=align.err # File to which STDERR will be written
 #SBATCH --mail-type=END,FAIL # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=asillers@ucdavis.edu # Email to which notifications will be$
 #SBATCH --time=8:00:00
