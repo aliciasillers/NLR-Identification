10a8910c243aafe68b490b59d40edc70d1bd8fab
asillers <asillers@ucdavis.edu>
2025-06-30 15:04:50 -0700

    entropy scripts finished and working

--- ShannonEntropy/entropy.R
+++ ShannonEntropy/entropy.R
@@ -1,9 +1,25 @@
 library(bio3d)
 library(tidyverse)
+library(stringr)
 
-aln <- read.fasta("nlr.aln.p.fa")
+samplename <- as.character(commandArgs(trailingOnly=TRUE))
+aln <- read.fasta(file = paste0(samplename))
+
+name <- str_split_i(samplename, coll("."), i = 1)
+name <- str_split_i(name, coll("/"), i = 2)
 
 ent <- entropy(aln)
 con <- consensus(aln)
 
+png(paste0("Groups/", name, ".png"), width = 15, height = 8, units = "cm", res = 900)
+plot1 <- barplot(ent$H, border = "white", ylab = "Entropy", space = 0, yaxt = "n")
+print(plot1)
+dev.off()
 
+n <- sum(ent$H >= 1.5)
+
+if (n >= 10){
+	write(name, "hvNLRs.txt", append = TRUE)
+} else {
+	write(name, "nonhvNLRs.txt", append = TRUE)
+}
--- ShannonEntropy/entropy.sh
+++ ShannonEntropy/entropy.sh
@@ -9,9 +9,12 @@
 #SBATCH --error=entropy.err # File to which STDERR will be written
 #SBATCH --mail-type=END,FAIL # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=asillers@ucdavis.edu # Email to which notifications will be$
-#SBATCH --time=8:00:00
+#SBATCH --time=4:00:00
+#SBATCH --array=1-31
 
 module load conda/latest
 conda activate nlrenv
 
-Rscript --vanilla --verbose entropy.R
+file=$(ls Groups/*.aln.fasta | sed -n ${SLURM_ARRAY_TASK_ID}p)
+
+Rscript --vanilla --verbose entropy.R $file
