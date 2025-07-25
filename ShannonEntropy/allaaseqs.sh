8dc55d2637ed37c6b14fe1dceb71ded4c4521821
asillers <asillers@ucdavis.edu>
2025-07-25 15:08:31 -0700

    script updates

--- ShannonEntropy/allaaseqs.sh
+++ ShannonEntropy/allaaseqs.sh
@@ -11,4 +11,4 @@
 #SBATCH --mail-user=asillers@ucdavis.edu # Email to which notifications will be$
 #SBATCH --time=2:00:00
 
-cat OtherSpecies/fv.rnl.protein.fa OtherSpecies/fv.cnl.protein.fa OtherSpecies/fv.tnl.protein.fa OtherSpecies/fv.nnl.protein.fa OtherSpecies/fv.other.protein.fa OtherSpecies/fi.rnl.protein.fa OtherSpecies/fi.cnl.protein.fa OtherSpecies/fi.tnl.protein.fa OtherSpecies/fi.nnl.protein.fa OtherSpecies/fi.other.protein.fa rnl.protein.fa cnl.protein.fa tnl.protein.fa nnl.protein.fa other.protein.fa Brilliance/rnl1.protein.fa Brilliance/cnl1.protein.fa Brilliance/tnl1.protein.fa Brilliance/nnl1.protein.fa Brilliance/other1.protein.fa Brilliance/rnl2.protein.fa Brilliance/cnl2.protein.fa Brilliance/tnl2.protein.fa Brilliance/nnl2.protein.fa Brilliance/other2.protein.fa > allaaseqs.fa
+cat OtherSpecies/fv.protein.fa OtherSpecies/fi.protein.fa ../rnl.protein.fa ../cnl.protein.fa ../tnl.protein.fa ../nnl.protein.fa ../other.protein.fa OtherSpecies/Brilliance/b1.protein.fa OtherSpecies/Brilliance/b2.protein.fa > allaaseqs.fa
