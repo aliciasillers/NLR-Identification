b114016735dd9129bb8d410aa765207c4305f41d
asillers <asillers@ucdavis.edu>
2025-09-18 11:46:14 -0700

    updated entropy scripts

--- ShannonEntropy/entropy.R
+++ ShannonEntropy/entropy.R
@@ -11,10 +11,15 @@ name <- str_split_i(name, coll("/"), i = 2)
 ent <- entropy(aln)
 con <- consensus(aln)
 
-png(paste0("Groups/", name, ".png"), width = 15, height = 8, units = "cm", res = 900)
-plot1 <- barplot(ent$H, border = "white", ylab = "Entropy", space = 0, yaxt = "n")
+names(ent$H)=con$seq
+head(ent$freq)
+
+png(paste0("RefinedTrees/", name, ".png"), width = 15, height = 8, units = "cm", res = 900)
+plot1 <- barplot(ent$H, ylab = "Entropy")
 print(plot1)
 dev.off()
+
+ent$H[ apply(ent$freq[21:22,],2,sum)>=0.6 ] = 0
 
 n <- sum(ent$H >= 1.5)
 
--- ShannonEntropy/entropy.sh
+++ ShannonEntropy/entropy.sh
@@ -3,18 +3,18 @@
 #SBATCH --job-name=entropy
 #SBATCH --ntasks=2 # Number of cores
 #SBATCH --nodes=1 # Ensure that all cores are on one machine
-#SBATCH --mem=12G # Memory pool for all cores in MB (see also --mem-per-cpu)
+#SBATCH --mem=16G # Memory pool for all cores in MB (see also --mem-per-cpu)
 #SBATCH --partition=bmh # Partition to submit to
 #SBATCH --output=entropy.out # File to which STDOUT will be written
 #SBATCH --error=entropy.err # File to which STDERR will be written
 #SBATCH --mail-type=END,FAIL # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=asillers@ucdavis.edu # Email to which notifications will be$
-#SBATCH --time=4:00:00
-#SBATCH --array=1-31
+#SBATCH --time=2:00:00
+#SBATCH --array=1-442
 
 module load conda/latest
 conda activate nlrenv
 
-file=$(ls Groups/*.aln.fasta | sed -n ${SLURM_ARRAY_TASK_ID}p)
+file=$(ls RefinedTrees/*.subali.afa | sed -n ${SLURM_ARRAY_TASK_ID}p)
 
 Rscript --vanilla --verbose entropy.R $file
