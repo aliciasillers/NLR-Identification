483400dd8e6b3b47d0bfaffabceb7386633de75a
asillers <asillers@ucdavis.edu>
2025-09-26 09:24:31 -0700

    script updates

--- ShannonEntropy/entropy.R
+++ ShannonEntropy/entropy.R
@@ -11,15 +11,17 @@ name <- str_split_i(name, coll("/"), i = 2)
 ent <- entropy(aln)
 con <- consensus(aln)
 
-names(ent$H)=con$seq
-head(ent$freq)
+#names(ent$H)=con$seq
+#head(ent$freq)
+
+ent$H[ apply(ent$freq[21:22,],2,sum)>=0.6 ] = 0
+
+names(ent$H) <- seq_along(1:length(ent$H))
 
 png(paste0("RefinedTrees/", name, ".png"), width = 15, height = 8, units = "cm", res = 900)
 plot1 <- barplot(ent$H, ylab = "Entropy")
 print(plot1)
 dev.off()
-
-ent$H[ apply(ent$freq[21:22,],2,sum)>=0.6 ] = 0
 
 n <- sum(ent$H >= 1.5)
 
