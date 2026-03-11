92c43b2d18d0a06d87296314f16f91408a92d55f
asillers <asillers@ucdavis.edu>
2026-03-11 15:53:26 -0700

    script updates

--- Selection/dnds_paralogs.R
+++ Selection/dnds_paralogs.R
@@ -8,7 +8,6 @@ library(data.table)
 
 #read and filter nlr files
 nlrdnds <- read.delim("NLR_paralogs.tsv", header = FALSE, sep = '\t')
-#alldnds$V3 <- as.numeric(alldnds$V3)
 dndsclean <- nlrdnds %>% filter(V4 <= 3) %>% filter(V5 <= 6)
 
 #read and filter control files
@@ -27,7 +26,7 @@ dnds$V6 <- factor(dnds$V6, level = c("RNL", "CNL", "TNL", "MNL", "Other", "CYP",
 
 #plot
 Y <- aggregate(V5~V6, data = dnds, max)[,2] + 0.15
-label <- c("a", "a", "a", "a", "a", "b", "c", "bc")#update with paralog stats results
+label <- c("ac", "ac", "a", "a", "ac", "b", "b", "bc")
 png("paralog_dnds1.png", width = 6.5, height = 4, units = "in", res = 900)
 plot1 <- ggplot(dnds, aes(x = factor(V6, level = c("RNL", "CNL", "TNL", "MNL", "Other", "CYP", "ACO", "OGDH")), y = V5, fill = factor(V6, level = c("RNL", "CNL", "TNL", "MNL", "Other", "CYP", "ACO", "OGDH")))) + geom_violin() + geom_jitter(width = 0.1, alpha = 0.3, size = 0.8) + scale_fill_manual(values = c(CNL = "thistle", TNL = "tomato", RNL = "lavenderblush", MNL ="goldenrod1", Other = "lightblue")) + labs(x = "", y = "dN/dS") + theme_minimal() + theme(legend.position="none") + stat_summary(fun = "mean", geom =  "point", color = "green4") + annotate("text", x = 1:8, y = Y, label = label)
 print(plot1)
@@ -45,8 +44,8 @@ plot2 <- ggplot(dnds, aes(x = V4, y = V5, color = V6)) + geom_point(alpha = 0.6)
 print(plot2)
 dev.off()
 dndsdt <- data.table(dnds)
-dndsdt <- dndsdt %>% mutate(dS = cut(V4, breaks=c(0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0)))
-png("paralog_ds_bins.png", width = 6.5, height = 5, units = "in", res = 900)
+dndsdt <- na.omit(dndsdt %>% mutate(dS = cut(V4, breaks=c(0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2))))
+png("paralog_ds_bins.png", width = 6.5, height = 2.5, units = "in", res = 900)
 plot3 <- ggplot(dndsdt, aes(x = factor(V6, level = c("RNL", "CNL", "TNL", "MNL", "Other", "CYP", "ACO", "OGDH")), y = V5, color = factor(V6, level = c("RNL", "CNL", "TNL", "MNL", "Other", "CYP", "ACO", "OGDH")))) + geom_violin() + geom_jitter(width = 0.1, alpha = 0.9, size = 0.8) + scale_color_manual(values = c(CNL = "thistle", TNL = "tomato", RNL = "lavenderblush", MNL ="goldenrod1", Other = "lightblue")) + labs(x = "", y = "dN/dS", fill = "") + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + theme(legend.position="none") + facet_wrap(vars(dS), ncol = 3, labeller = "label_both") + stat_summary(fun = "mean", geom =  "point", color = "green4", size = 0.9)
 print(plot3)
 dev.off()
